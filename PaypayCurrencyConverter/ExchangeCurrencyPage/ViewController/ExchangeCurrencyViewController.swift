f27b07bf72886e348de456bec2999c0bb7e84379
HsinChungHan <hooy123456@gapp.nthu.edu.tw>
2021-03-08 01:48:36 +0800

    Use a FlowVC to manage present action

--- PaypayCurrencyConverter/ExchangeCurrencyPage/ViewController/ExchangeCurrencyViewController.swift
+++ PaypayCurrencyConverter/ExchangeCurrencyPage/ViewController/ExchangeCurrencyViewController.swift
@@ -8,14 +8,29 @@
 import UIKit
 import SnapKit
 
+protocol ExchangeCurrencyViewControllerFlowDelegate: AnyObject {
+    func exchangeCurrencyViewControllerFlowDelegateGoToDisplayCurrenciesViewModel(_ exchangeCurrencyViewController: ExchangeCurrencyViewController, amountCurrency: AmountCurrency)
+}
+
 class ExchangeCurrencyViewController: UIViewController {
 
     let viewModel = ExchangeCurrencyVCViewModel()
+    
+    weak var flowDelegate: ExchangeCurrencyViewControllerFlowDelegate?
     
     lazy var inputAmountTextfiled = makeInputAmountTextfiled()
     lazy var currencyLable = makeCurrencyLable()
     lazy var exchangeCurrencyButton = makeExchangeCurrencyButton()
     lazy var currenciesPickeView = makeCurrenciesPickeView()
+    
+    init(flowDelegate: ExchangeCurrencyViewControllerFlowDelegate) {
+        self.flowDelegate = flowDelegate
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
